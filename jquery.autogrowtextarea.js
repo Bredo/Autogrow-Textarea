4a2367d72d1e194196dba4d0376f182f9627ba31
Jonathan Bredo <jonathanbredo@icloud.com>
2013-05-05 18:57:56 +0200

    Version 3.1.1: CSS3 columns support

--- jquery.autogrowtextarea.js
+++ jquery.autogrowtextarea.js
@@ -1,60 +1,56 @@
 /*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <jevin9@gmail.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return. Jevin O. Sewaruth
- * ----------------------------------------------------------------------------
- *
- * Autogrow Textarea Plugin Version v3.0
- * http://www.technoreply.com/autogrow-textarea-plugin-3-0
- * 
- * THIS PLUGIN IS DELIVERD ON A PAY WHAT YOU WHANT BASIS. IF THE PLUGIN WAS USEFUL TO YOU, PLEASE CONSIDER BUYING THE PLUGIN HERE :
- * https://sites.fastspring.com/technoreply/instant/autogrowtextareaplugin
- *
- * Date: October 15, 2012
+ * Autogrow v3.1 by Jonathan Bredo
+ * Forked jQuery plugin from jevin @ GitHub.com.
+ * Date: May 5th, 2013
+ * Original note:
+	* ----------------------------------------------------------------------------
+	* "THE BEER-WARE LICENSE" (Revision 42):
+	* <jevin9@gmail.com> wrote this file. As long as you retain this notice you
+	* can do whatever you want with this stuff. If we meet some day, and you think
+	* this stuff is worth it, you can buy me a beer in return. Jevin O. Sewaruth
+	* ----------------------------------------------------------------------------
+	*
+	* Autogrow Textarea Plugin Version v3.0
+	* http://www.technoreply.com/autogrow-textarea-plugin-3-0
+	* 
+	* THIS PLUGIN IS DELIVERD ON A PAY WHAT YOU WHANT BASIS. IF THE PLUGIN WAS USEFUL TO YOU, PLEASE CONSIDER BUYING THE PLUGIN HERE :
+	* https://sites.fastspring.com/technoreply/instant/autogrowtextareaplugin
+	*
+	* Date: October 15, 2012
  */
 
-jQuery.fn.autoGrow = function() {
+$.fn.autoGrow = function() {
 	return this.each(function() {
-
 		var createMirror = function(textarea) {
-			jQuery(textarea).after('<div class="autogrow-textarea-mirror"></div>');
-			return jQuery(textarea).next('.autogrow-textarea-mirror')[0];
+			$(textarea).after('<div class="ag-mirror"></div>');
+			return $(textarea).next('.ag-mirror')[0];
 		}
-
 		var sendContentToMirror = function (textarea) {
 			mirror.innerHTML = String(textarea.value).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br />') + '.<br/>.';
-
-			if (jQuery(textarea).height() != jQuery(mirror).height())
-				jQuery(textarea).height(jQuery(mirror).height());
+			if ($(textarea).height() != $(mirror).height()){
+				$(textarea).height($(mirror).height());
+			}
 		}
-
+		var updateStyles = function () {
+			$(mirror).css("display", "none");
+			$(mirror).css("wordwrap", $(textarea).css("wordwrap"));
+			$(mirror).css("padding", $(textarea).css("padding"));
+			$(mirror).css("width", $(textarea).css("width"));
+			$(mirror).css("font-family", $(textarea).css("font-family"));
+			$(mirror).css("font-size", $(textarea).css("font-size"));
+			$(mirror).css("line-height", $(textarea).css("line-height"));
+			$(mirror).css("-webkit-columns", $(textarea).css("-webkit-columns"));
+			$(mirror).css("-moz-columns", $(textarea).css("-moz-columns"));
+			$(mirror).css("columns", $(textarea).css("columns"));
+		}
 		var growTextarea = function () {
 			sendContentToMirror(this);
+			updateStyles(this);
 		}
-
-		// Create a mirror
 		var mirror = createMirror(this);
-		
-		// Style the mirror
-		mirror.style.display = 'none';
-		mirror.style.wordWrap = 'break-word';
-		mirror.style.padding = jQuery(this).css('padding');
-		mirror.style.width = jQuery(this).css('width');
-		mirror.style.fontFamily = jQuery(this).css('font-family');
-		mirror.style.fontSize = jQuery(this).css('font-size');
-		mirror.style.lineHeight = jQuery(this).css('line-height');
-
-		// Style the textarea
 		this.style.overflow = "hidden";
 		this.style.minHeight = this.rows+"em";
-
-		// Bind the textarea's event
 		this.onkeyup = growTextarea;
-
-		// Fire the event for text already present
-		sendContentToMirror(this);
-
+		growTextarea(this);
 	});
 };
